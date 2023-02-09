1b66e26050fb4ebce54ee7042e840c9dac88bf6d
Chalfe <kewin.paepke@gmail.com>
2023-02-09 10:42:27 -0800

    Particle Thingy

--- Name-Pending-Game/Assets/Scripts/CameraChange.cs
+++ Name-Pending-Game/Assets/Scripts/CameraChange.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraChange : MonoBehaviour
 {
-    public GameObject Camera;
-    public bool Entered;
+    public GameObject MainCamera;
+    private bool Entered;
+    public bool Cut;
+    private CinemachineBrain brain;
+
+
     // Start is called before the first frame update
     void Start()
     {
         Entered = false;
+
     }
 
     // Update is called once per frame
@@ -21,12 +27,33 @@ public class CameraChange : MonoBehaviour
     {
         if(Entered == false)
         {
-            Camera.SetActive(false);
+            if(Cut == true)
+            {
+                brain = FindObjectOfType<CinemachineBrain>();
+                brain.m_DefaultBlend.m_Time = 0;
+            }
+            else
+            {
+                brain.m_DefaultBlend.m_Time = 1;
+            }
+
+            MainCamera.SetActive(false);
             Entered = true;
         }
         else
         {
-            Camera.SetActive(true);
+
+            if (Cut == true)
+            {
+                brain = FindObjectOfType<CinemachineBrain>();
+                brain.m_DefaultBlend.m_Time = 0;
+            }
+            else
+            {
+                brain.m_DefaultBlend.m_Time = 1;
+            }
+
+            MainCamera.SetActive(true);
             Entered = false;
         }
 
