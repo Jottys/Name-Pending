159e932a9acef1fef36a2e18ea8dc83e2a2e0feb
Blinky <emiiru.re@gmail.com>
2023-02-09 17:15:08 -0800

    i did this so i can maybe work

---
+++ Name-Pending-Game/Assets/Scripts/Movement/Player_Controller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    #region Player Speeds
+    [SerializeField]
+    private float _playerSpeed = 5f;
+    #endregion
+
+    private Vector2 _playerVelocity;
+
+
+    private void Update()
+    {
+
+        
+    }
+}
